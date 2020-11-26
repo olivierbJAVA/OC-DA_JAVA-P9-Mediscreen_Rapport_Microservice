ba1bae76c952045d12935ab4ef4fff1a99874fbe
olivier.bonheur <olivier.bonheur@gmail.com>
2020-11-26 15:38:00 +0100

    Work on Service layer

--- src/main/java/com/mediscreen/rapport/service/IRapportService.java
+++ src/main/java/com/mediscreen/rapport/service/IRapportService.java
@@ -1,7 +1,12 @@
 package com.mediscreen.rapport.service;
 
+import com.mediscreen.rapport.domain.Rapport;
+
 /**
  * Interface to be implemented to manage the services for Rapport entities.
  */
 public interface IRapportService {
+
+    Rapport getRapport(String lastName, String firstName);
+
 }
--- src/main/java/com/mediscreen/rapport/service/RapportServiceImpl.java
+++ src/main/java/com/mediscreen/rapport/service/RapportServiceImpl.java
@@ -1,10 +1,41 @@
 package com.mediscreen.rapport.service;
 
+import com.mediscreen.rapport.domain.Note;
+import com.mediscreen.rapport.domain.Patient;
+import com.mediscreen.rapport.domain.Rapport;
+import com.mediscreen.rapport.proxy.NoteMicroserviceProxy;
+import com.mediscreen.rapport.proxy.PatientMicroserviceProxy;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 /**
  * Class in charge of managing the services for Rapport entities.
  */
 @Service
 public class RapportServiceImpl implements IRapportService {
+
+    private PatientMicroserviceProxy patientProxy;
+    private NoteMicroserviceProxy noteProxy;
+
+    public RapportServiceImpl(PatientMicroserviceProxy patientProxy, NoteMicroserviceProxy noteProxy) {
+        this.patientProxy = patientProxy;
+        this.noteProxy = noteProxy;
+    }
+
+    @Override
+    public Rapport getRapport(String lastName, String firstName) {
+
+        Patient patient = patientProxy.getPatientByLastNameAndFirstName(lastName, firstName);
+
+        List<Note> notes = noteProxy.getNotesByLastNameAndFirstName(lastName, firstName);
+
+        Rapport rapport = new Rapport(patient.getLastName(), patient.getFirstName(), patient.getDateOfBirth(), patient.getSex());
+        rapport.setNotes(notes);
+
+        System.out.println("rapport = " + rapport.toString() );
+
+        return rapport;
+    }
+
 }
