bbb776b8df2e13253bc8141a7a2f80b7a661f0c5
olivier.bonheur <olivier.bonheur@gmail.com>
2020-11-26 15:39:05 +0100

    Work on Application layer

--- src/main/java/com/mediscreen/rapport/RapportApplication.java
+++ src/main/java/com/mediscreen/rapport/RapportApplication.java
@@ -2,11 +2,13 @@ package com.mediscreen.rapport;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 /**
  * Class in charge of launching the Rapport Microservices in charge of producing patient diabetes risk assessment report.
  */
 @SpringBootApplication
+@EnableFeignClients("com.mediscreen.rapport")
 public class RapportApplication {
 
 	public static void main(String[] args) {
