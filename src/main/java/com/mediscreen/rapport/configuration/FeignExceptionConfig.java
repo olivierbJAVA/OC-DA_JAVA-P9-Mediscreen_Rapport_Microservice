68827e391df45c2ed5972d29e0e6b710bf5e3e0c
olivier.bonheur <olivier.bonheur@gmail.com>
2020-11-29 23:49:10 +0100

    Work on the JavaDoc

--- src/main/java/com/mediscreen/rapport/configuration/FeignExceptionConfig.java
+++ src/main/java/com/mediscreen/rapport/configuration/FeignExceptionConfig.java
@@ -4,6 +4,9 @@ import com.mediscreen.rapport.exception.CustomErrorDecoder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+/**
+ * Configuration class for Feign exceptions management.
+ */
 @Configuration
 public class FeignExceptionConfig {
 
--- src/main/java/com/mediscreen/rapport/exception/CustomErrorDecoder.java
+++ src/main/java/com/mediscreen/rapport/exception/CustomErrorDecoder.java
@@ -3,10 +3,20 @@ package com.mediscreen.rapport.exception;
 import feign.Response;
 import feign.codec.ErrorDecoder;
 
+/**
+ * Class managing Feign custom errors.
+ */
 public class CustomErrorDecoder implements ErrorDecoder {
 
     private final ErrorDecoder defaultErrorDecoder = new ErrorDecoder.Default();
 
+    /**
+     * Manage Feign custom exceptions.
+     *
+     * @param invoker the invoker for the exception
+     * @param response the error response
+     * @return The default error decoder
+     */
     @Override
     public Exception decode(String invoker, Response response) {
 
