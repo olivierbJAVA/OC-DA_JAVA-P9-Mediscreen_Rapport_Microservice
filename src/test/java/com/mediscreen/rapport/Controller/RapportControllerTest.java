3ebeaba56c11a82e203b73d6e0c37940bb6c516d
olivier.bonheur <olivier.bonheur@gmail.com>
2020-12-04 23:30:36 +0100

    Some cleaning

--- src/test/java/com/mediscreen/rapport/Controller/RapportControllerTest.java
+++ src/test/java/com/mediscreen/rapport/Controller/RapportControllerTest.java
@@ -4,7 +4,6 @@ import com.mediscreen.rapport.constant.Assessment;
 import com.mediscreen.rapport.constant.Sex;
 import com.mediscreen.rapport.controller.RapportController;
 import com.mediscreen.rapport.domain.Rapport;
-import com.mediscreen.rapport.exception.ResourceNotFoundException;
 import com.mediscreen.rapport.service.IRapportService;
 import org.junit.jupiter.api.Test;
 import org.slf4j.Logger;
@@ -36,7 +35,7 @@ public class RapportControllerTest {
     private IRapportService mockRapportService;
 
     @Test
-    public void getRapportByLastNameAndFirstName_whenLastNameAndFirstNameExist() {
+    public void getRapportByLastNameAndFirstName() {
         //ARRANGE
         Rapport rapportToGet = new Rapport("PatientTestLastName", "PatientTestFirstName", Sex.M, 50L, Assessment.None);
         doReturn(rapportToGet).when(mockRapportService).getRapportByLastNameAndFirstName("PatientTestLastName", "PatientTestFirstName");
@@ -54,25 +53,7 @@ public class RapportControllerTest {
     }
 
     @Test
-    public void getRapportByLastNameAndFirstName_whenLastNameAndFirstNameNotExist() {
-        //ARRANGE
-        doThrow(ResourceNotFoundException.class).when(mockRapportService).getRapportByLastNameAndFirstName("PatientTestLastName","PatientTestFirstName");
-
-        //ACT & ASSERT
-        try {
-            mockMvc.perform(get("/assess/lastNameAndFirstName")
-                    .param("lastName","PatientTestLastName")
-                    .param("firstName","PatientTestFirstName"))
-                    .andExpect(status().isNotFound());
-        } catch (Exception e) {
-            logger.error("Error in MockMvc", e);
-        }
-
-        verify(mockRapportService, times(1)).getRapportByLastNameAndFirstName("PatientTestLastName","PatientTestFirstName");
-    }
-
-    @Test
-    public void getRapportById_whenIdExist() {
+    public void getRapportById() {
         //ARRANGE
         Rapport rapportToGet = new Rapport();
         rapportToGet.setLastName("PatientTestLastName");
@@ -93,20 +74,4 @@ public class RapportControllerTest {
         verify(mockRapportService, times(1)).getRapportById(1L);
     }
 
-    @Test
-    public void getRapportById_whenIdNotExist() {
-        //ARRANGE
-        doThrow(ResourceNotFoundException.class).when(mockRapportService).getRapportById(1L);
-
-        //ACT & ASSERT
-        try {
-            mockMvc.perform(get("/assess/id")
-                    .param("id","1"))
-                    .andExpect(status().isNotFound());
-        } catch (Exception e) {
-            logger.error("Error in MockMvc", e);
-        }
-
-        verify(mockRapportService, times(1)).getRapportById(1L);
-    }
 }
